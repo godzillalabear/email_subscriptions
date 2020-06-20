71b4e1aae73d63537f33fd809062830bcdbbbf47
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-06-20 14:50:14 +0800

    feature: make project and task form prettier

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -26,6 +26,7 @@ class ProjectsController < ApplicationController
   # POST /projects.json
   def create
     @project = Project.new(project_params)
+    @project.user = current_user
 
     respond_to do |format|
       if @project.save
