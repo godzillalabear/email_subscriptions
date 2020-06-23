43c245f97ec93b6e5a83d5e006c226c684134ddf
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-06-23 12:11:55 +0800

    feature: add task mailer method task_created 1. send mail to users(except the author) in the project when a task is created 2. set the params in task controller that mailer needed 3. set the instance for mailer to give view 4. set the email receiver and subject in project mailer 5. write the mail content in views/task_mailer/task_created.html.erb 6. preview what will the mail look like 7. delete text form of the mail

---
+++ test/mailers/previews/task_mailer_preview.rb
@@ -0,0 +1,16 @@
+# test/mailers/previews/task_mailer_preview.rb
+
+# Preview all emails at http://localhost:3000/rails/mailers/task_mailer
+class TaskMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/task_mailer/task_created
+  def task_created
+    TaskMailer.with(task: Task.last, user: User.first, author: User.last).task_created
+  end
+
+  # Preview this email at http://localhost:3000/rails/mailers/task_mailer/task_completed
+  def task_completed
+    TaskMailer.task_completed
+  end
+
+end
