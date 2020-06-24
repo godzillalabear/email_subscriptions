636e60d34b930804d41822ce00a543f35c8677ae
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-06-24 10:32:58 +0800

    feature: when user click the unsubscribe link in email, email controller will update the notification columns

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,4 +1,17 @@
 class EmailsController < ApplicationController
   def unsubscribe
+    user = User.find_by_unsubscribe_hash(params[:unsubscribe_hash])
+
+    case params[:subscription]
+      when "user_added_to_project"
+        @reason = "being added to project"
+        user.update(notify_when_added_to_project: false)
+      when "task_created"
+        @reason = "new tasks"
+        user.update(notify_when_task_created: false)
+      when "task_completed"
+        @reason = "completing task"
+        user.update(notify_when_task_completed: false)
+    end
   end
 end
